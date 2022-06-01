3a36a2e6cdde628c87c6a8c2aa92aae8a9ad49b1
LinearRegression <38725186+BlackboardYang@users.noreply.github.com>
2022-06-01 16:04:07 +0900

    Security deleted

---
+++ NewMall/GroupAMall/src/main/java/com/GroupAMall/GroupAMallApplication.java
@@ -0,0 +1,13 @@
+package com.GroupAMall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupAMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupAMallApplication.class, args);
+	}
+
+}
