263b323d1886dbb9633f607a85e95cbaba486a0b
LinearRegression <38725186+BlackboardYang@users.noreply.github.com>
2022-06-10 13:32:55 +0900

    BrandController updated

--- Mall/src/main/java/com/mall/controller/PmsBrandController.java
+++ Mall/src/main/java/com/mall/controller/PmsBrandController.java
@@ -1,5 +1,8 @@
 package com.mall.controller;
 
+import java.util.Map;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.mall.model.pms.PmsBrand;
 import com.mall.model.response.CommonResult;
 import com.mall.repository.pms.PmsBrandRepository;
+import com.mall.service.PmsBrandService;
 
 @RequestMapping("/brand")
 @Controller
@@ -30,6 +34,15 @@ public class PmsBrandController {
 
 	}
 
+	@ResponseBody
+	@RequestMapping(value = "/listAll", method = RequestMethod.GET)
+	public CommonResult brandListAll(@RequestParam() Map<String, Object> map) {
+		map.put("", pmsBrandService.findAllBrands());
+		
+		return new CommonResult(200, map, "ok");
+	}
+
+	// TODO 等会再写
 	// get商品品牌下拉栏
 	@ResponseBody
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
@@ -50,5 +63,29 @@ public class PmsBrandController {
 
 		return new CommonResult(200, request, "ok");
 	}
+	
+//		// logger.info("get brand request {}", request);
+//		List<PmsBrand> pmsBrandList = pmsBrandParam.findAll();
+//		logger.info("get brand repository {}", pmsBrandParam);
+//		// TODO 把数据传到添加商品的品牌选项下拉栏
+//		// logger.info("get brand repository {}", pmsBrandRepository);
+//		// logger.info("get copied brand request {}", request);
+////			PmsBrand pmsBrand = PmsBrand.builder()//
+////					.brandId(request.getParameter("brandId") == null ? null : Long.valueOf(request.getParameter("brandId")))//
+////					.brandName(request.getParameter("brandName") == null ? null : String.valueOf(request.getParameter("brandName"))).build();
+////			CommonPagePmsProduct cppp = new CommonPagePmsProduct(products, 0, 0, 0l, 0);
+//		PmsBrandParam pmsBrandParam;
+//		
+//		//TODO if 页码的if判断
+////		if(pageSize * pageNum < products.size()) { 
+////			products = products.subList(pageNum * pageSize -  pageSize, pageNum * pageSize);
+////		} 
+////		if(pageNum * pageSize >= products.size() && pageNum * pageSize - pageSize != 0 && products.size() > pageNum * pageSize - pageSize) {
+////			products = products.subList(pageNum * pageSize -  pageSize, products.size());
+////		}
+//		
+//		CommonResultPmsBrand commonResultPmsBrand = new CommonResultPmsBrand<>((pmsBrandList, pageNum, pageSize, total, totalPage));
+//		return new CommonResult(200, commonResultPmsBrand, "ok");
+//	}
 
 }
