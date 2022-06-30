a277fbc77ab39cb3bb376e19470be93def8e209e
LinearRegression <38725186+BlackboardYang@users.noreply.github.com>
2022-06-30 14:32:07 +0900

    pmsProductList method fixed

--- Mall/src/main/java/com/mall/service/PmsProductService.java
+++ Mall/src/main/java/com/mall/service/PmsProductService.java
@@ -1,11 +1,17 @@
 package com.mall.service;
 
+import java.util.List;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.mall.model.param.PmsProductParam;
 import com.mall.model.pms.PmsProduct;
+import com.mall.model.response.CommonPage;
 import com.mall.repository.pms.PmsProductRepository;
 
 @Service
@@ -32,5 +38,28 @@ public class PmsProductService {
 		BeanUtils.copyProperties(param, pmsProduct);
 		pmsProductRepository.save(pmsProduct);
 
+	}
+
+	public List<PmsProduct> findAllProducts() {
+		return pmsProductRepository.findAll();
+	}
+
+	public CommonPage<PmsProduct> findRequiredProducts(Integer pageNum, Integer pageSize, String keyword) {
+
+		CommonPage<PmsProduct> commonPage;
+		Pageable pageable = PageRequest.of(pageNum - 1, pageSize);
+		List<PmsProduct> productsList;
+		Page<PmsProduct> products;
+
+		if (keyword == null) {
+			products = pmsProductRepository.findAll(pageable);
+		} else {
+			products = pmsProductRepository.findByNameLike(pageable, ("%" + keyword + "%"));
+		}
+		productsList = products.getContent();
+
+		commonPage = new CommonPage<PmsProduct>(productsList, pageNum, pageSize, products.getTotalElements(),
+				products.getTotalPages());
+		return commonPage;
 	}
 }
