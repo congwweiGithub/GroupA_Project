4129099c651a3119fc56ee9a0b0ffc8eb8c2f9f6
congwweiGithub <congwei@k-stamp.co.jp>
2022-06-30 09:55:38 +0900

    删除没用的文件

---
+++ Mall/src/main/java/com/mall/model/pms/PmsProductAttribute.java
@@ -0,0 +1,48 @@
+//TODO
+
+//package com.mall.model.pms;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//@Entity
+//@Builder
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Data
+//public class PmsProductAttribute {
+//
+//	private Integer filterType;
+//
+//	private Integer handAddStatus;
+//
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.AUTO)
+//	private Long id;
+//
+//	private String inputList;
+//
+//	private Integer inputType;
+//
+//	private String name;
+//
+//	private Long productAttributeCategoryId;
+//
+//	private Integer relatedStatus;
+//
+//	private Integer searchType;
+//
+//	private Integer selectType;
+//
+//	private Integer sort;
+//
+//	private Integer type;
+//
+//}
